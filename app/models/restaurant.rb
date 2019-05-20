981d080a340b9bf09252bba576c093e95b82a654
Guillaume Casile <g.casile@gmail.com>
2019-05-20 16:17:42 +0200

    etape MC fin

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,9 +1,11 @@
+CATEGORIES = %w[chinese italian japanese french belgian]
+
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
   validates :name, presence: true
   validates :phone_number, presence: true
   validates :address, presence: true
   validates :category, inclusion: {
-    in: %w[chinese italian japanese french belgian]
+    in: CATEGORIES
   }
 end
